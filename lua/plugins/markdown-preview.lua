7a342aff33bdb42a7491f015e2b9603915640d67
Albert24GG <guiman.albert09@gmail.com>
2024-12-14 00:24:49 +0200

    Use astrocommunity package for markdown preview

--- lua/plugins/markdown-preview.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = ":call mkdp#util#install()",
-    ft = { "markdown" },
-}
