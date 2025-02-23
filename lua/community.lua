95eb4c41acd2318311a6e683f8d823983ae4882b
Albert24GG <guiman.albert09@gmail.com>
2025-02-24 00:59:29 +0200

    Replace `leap.nvim` with `flash.nvim`

--- lua/community.lua
+++ lua/community.lua
@@ -15,7 +15,6 @@ return {
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.fish" },
-  -- { import = "astrocommunity.completion.codeium-vim" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.ultimate-autopair-nvim" },
@@ -23,16 +22,11 @@ return {
   { import = "astrocommunity.editing-support.telescope-undo-nvim" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.indent.indent-blankline-nvim" },
-  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.register.nvim-neoclip-lua" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.keybinding.nvcheatsheet-nvim" },
-  -- { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
-  -- { import = "astrocommunity.utility.noice-nvim" },
-  -- { import = "astrocommunity.lsp.nvim-lint" },
-  -- { import = "astrocommunity.editing-support.conform-nvim" },
-
   -- import/override with your plugins folder
 }
