29c992c2e54131f85319ec9dafb676af4a6863db
Albert24GG <guiman.albert09@gmail.com>
2026-01-04 11:43:37 +0200

    Add smart-splits resizing mappings

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -12,5 +12,14 @@ return {
         scrolloff = 5,
       },
     },
+
+    mappings = {
+      n = {
+        ["<A-h>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" },
+        ["<A-j>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" },
+        ["<A-k>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" },
+        ["<A-l>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" },
+      },
+    },
   },
 }
