393900c0f599750e6dc306a83f07e7470c764c84
Albert24GG <guiman.albert09@gmail.com>
2026-04-20 10:56:13 +0300

    Update to v6

--- lua/community.lua
+++ lua/community.lua
@@ -9,7 +9,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.go" },
